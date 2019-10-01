72e89e037043b11235150e03a2d0a72329d9fedd
zombityu <vagasi.ityu@gmail.com>
2019-10-01 17:19:19 +0200

    add : (imageservice,upload)

---
+++ Azuretestproject/Azuretestproject/Services/ImageService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Azuretestproject.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IConfiguration _configuration;
+        private CloudBlobClient _blobClient;
+        private CloudBlobContainer _blobContainer;
+
+        public ImageService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<CloudBlobContainer> GetBlobContainer()
+        {
+            if (_blobContainer != null)
+                return _blobContainer;
+
+            var containerName = "hotelphotos";
+            if (string.IsNullOrWhiteSpace(containerName))
+            {
+                throw new ArgumentException("Configuration must contain ContainerName");
+            }
+
+            var blobClient = GetClient();
+
+            _blobContainer = blobClient.GetContainerReference(containerName);
+            if (await _blobContainer.CreateIfNotExistsAsync())
+            {
+                await _blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            }
+            return _blobContainer;
+        }
+
+        public async Task UploadAsync(IFormFileCollection files)
+        {
+            var blobcontainer =await GetBlobContainer();
+            for (int i = 0; i < files.Count; i++)
+            {
+                var blob = blobcontainer.GetBlockBlobReference(GetRandomBlobName(files[i].FileName));
+                using (var stream = files[i].OpenReadStream())
+                {
+                    await blob.UploadFromStreamAsync(stream);
+                }
+            }
+        }
+
+        private CloudBlobClient GetClient()
+        {
+            if (_blobClient != null)
+                return _blobClient;
+
+            var storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=testphotos;AccountKey=XmgkCr7+8migTd1u8Y9rO1Uag9fGyLKkcwgoTcBv6jNtD/OnDGYclnlSI6cXfg5E75i3u2xkpanv6XsEMb2agA==;EndpointSuffix=core.windows.net";
+            if (string.IsNullOrWhiteSpace(storageConnectionString))
+            {
+                throw new ArgumentException("Configuration must contain StorageConnectionString");
+            }
+
+            if (!CloudStorageAccount.TryParse(storageConnectionString, out CloudStorageAccount storageAccount))
+            {
+                throw new Exception("Could not create storage account with StorageConnectionString configuration");
+            }
+
+            _blobClient = storageAccount.CreateCloudBlobClient();
+            return _blobClient;
+        }
+
+        private string GetRandomBlobName(string filename)
+        {
+            string ext = Path.GetExtension(filename);
+            return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
+        }
+    }
+}
